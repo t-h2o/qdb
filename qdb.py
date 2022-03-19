31d78c92c89552ef90e72b3a6530ed850c8a52ba
hypn0x <leetpwnsroot@gmail.com>
2022-03-19 16:27:05 +0100

    Add a nice prompt && hook

--- qdb.py
+++ qdb.py
@@ -1,3 +1,26 @@
-import lldb
 
-print("\nHello lldb this is qdb...\n")
+# Import libraries
+import lldb
+import sys
+import os
+#import time
+
+
+VERSION = "1.0"
+
+def __lldb_init_module(debugger, internal_dict):
+    
+    res = lldb.SBCommandReturnObject()
+    output = debugger.GetCommandInterpreter()
+
+    output.HandleCommand("settings set prompt \"(qdb) \"", res) 
+    return
+
+#-----------------------------------
+# main
+#-----------------------------------
+    
+
+print("===============")
+print("QDB vesrion", VERSION)
+print("===============")
